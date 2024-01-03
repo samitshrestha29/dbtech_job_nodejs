5ef190511c524bdf24b97174dc6ca9e3149af6b4
samitshrestha29 <samitshrestha29@gmail.com>
2024-01-03 13:25:06 +0545

    login operation password hidden

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,18 +6,33 @@ module.exports = {
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      //   CryptoJS.AES.encrypt(req.body.password, process.env.SECRET);
-
-      password: CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.SECRET
-      ).toString(),
+      password: req.body.password,
     });
     try {
       const savedUser = await newUser.save();
       res.status(201).json(savedUser);
     } catch (error) {
       res.status(500).json(error);
+    }
+  },
+
+  loginUser: async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      !user && res.status(401).json("wrong login Detail");
+
+      const decryptedPass = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.SECRET
+      );
+      const decryptedPassword = decryptedPass.toString(CryptoJS.enc.Utf8);
+
+      decryptedPassword !== req.body.password &&
+        res.status(401).json("wrong password");
+
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500);
     }
   },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 
+// Correct import statement
 const authController = require("../controllers/authController");
 
 // REGISTRATION
 router.post("/register", authController.createUser);
+
+// // Login
+router.post("/login", authController.loginUser);
 
 module.exports = router;
