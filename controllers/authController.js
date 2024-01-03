5dd7e0da91960c72704a026eca6827cb71f9549b
samitshrestha29 <samitshrestha29@gmail.com>
2024-01-03 11:31:59 +0545

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 module.exports = {
   createUser: async (req, res) => {
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      //   CryptoJS.AES.encrypt(req.body.password, process.env.SECRET);
+
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET
+      ).toString(),
     });
     try {
       const savedUser = await newUser.save();
